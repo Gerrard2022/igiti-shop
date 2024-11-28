1df66fb5482c05329e0ba2005256e90ee4c347b1
KABERUKA Gerrard <110633144+Gerrard2022@users.noreply.github.com>
2024-11-28 16:53:13 +0200

    Update page.tsx

--- app/(routes)/search/page.tsx
+++ app/(routes)/search/page.tsx
@@ -19,7 +19,7 @@ const SearchPage = () => {
 
   const { data, error } = useSWR(
     () => [
-      `/api/${process.env.NEXT_PUBLIC_API_URL}/products`,
+      `${process.env.NEXT_PUBLIC_API_URL}/products`,
       {
         name:
           searchQuery && searchQuery.length >= 3 ? encodeURI(searchQuery) : "",
